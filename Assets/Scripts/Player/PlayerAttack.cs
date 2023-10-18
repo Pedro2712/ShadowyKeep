5d7bccb6e8bd3d741dfa45a7daa758120af0a4e3
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-10-18 15:12:15 -0300

    Update player animations, add idles and attack animations.

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Animator animator;
+    void OnSimpleSwordAttack(){
+        print("Attack Pressed");
+        animator.SetTrigger("swordAttack");
+    }
+
+
+}
