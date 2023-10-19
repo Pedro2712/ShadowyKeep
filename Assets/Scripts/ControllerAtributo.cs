c34b7ee4b5de58e49136454537f2fe5c7b5a4dc1
Pedro2712 <pedrohenriqueandrade2712@gmail.com>
2023-10-19 20:23:28 -0300

    tela de informações feita

---
+++ Assets/Scripts/ControllerAtributo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerAtributo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator animator;
+    void Start()
+    {
+        animator= GetComponent<Animator>();
+    }
+
+    public void Open() {
+        animator.SetBool("isOpen", true);
+    }
+
+    public void Close() {
+        animator.SetBool("isOpen", false);
+    }
+}
