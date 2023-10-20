5e1e1cdc0f6019ad56075b754307dc4952c5b1c6
leticiacb1 <leticiacb1@al.insper.edu.br>
2023-10-20 01:55:39 -0300

    Add script and respawn points to all rooms.

--- Assets/Scripts/Inimigos/WaveSpawner.cs
+++ Assets/Scripts/Inimigos/WaveSpawner.cs
@@ -18,51 +18,77 @@ public class WaveSpawner : MonoBehaviour
     public int qtd_enemies; // Quantidade de inimigos que deveram ser gerados
 
     //Controlando a onda por tempo:
-    public Transform spawnLocation;
-    public int waveDuration;
-    private float waveTimer;
-    private float spawnInterval;
-    private float spawnTimer;
+    public List<Transform> spawnLocations;
 
+    public List<Transform> selectedSpawnLocations;
+    // public int waveDuration;
+    // private float waveTimer;
+    // private float spawnInterval;
+    // private float spawnTimer;
 
     public List<GameObject> enemiesToSpawn = new List<GameObject>();
     void Start()
     {
         GenerateWave();
+
+        // Seleciona um número de posições de forma aletória.
+        // Sempre tenho mais opções de Respawn do que inimigos para spawnar.
+        Debug.LogFormat("Teste");
+        int count_location = 0;
+        List<int> locationSelectedId = new List<int>();
+
+        while(count_location < enemiesToSpawn.Count){
+            int randLocationId = Random.Range(0, spawnLocations.Count);
+            
+            if (!locationSelectedId.Contains(randLocationId))
+            {
+                selectedSpawnLocations.Add(spawnLocations[randLocationId]);
+                locationSelectedId.Add(randLocationId);
+                count_location+=1;
+            }
+        }
+        
+        int count_enemies = 0;
+        while(enemiesToSpawn.Count > 0 ){
+            Instantiate(enemiesToSpawn[count_enemies], selectedSpawnLocations[count_enemies].position, Quaternion.identity);
+            // Remove o inimigo da lista de Spawn
+            enemiesToSpawn.RemoveAt(0);
+            count_enemies+=1;
+        }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        if(spawnTimer <= 0 )
-        {
-            //Spawna um inimigo
+    // void FixedUpdate()
+    // {
+    //     if(spawnTimer <= 0 )
+    //     {
+    //         //Spawna um inimigo
 
-            if(enemiesToSpawn.Count > 0 ){ // Enquanto tenho inimigos para Spawnar
-                Instantiate(enemiesToSpawn[0], spawnLocation.position, Quaternion.identity);
-                // Remove o inimigo da lista de Spawn
-                enemiesToSpawn.RemoveAt(0);
-                spawnTimer = spawnInterval;
-            }
-            else
-            {
-                waveTimer = 0;
-            }
-        }
-        else
-        {
-            spawnTimer -= Time.fixedDeltaTime;
-            waveTimer -= Time.fixedDeltaTime;
-        }
-    }
+    //         if(enemiesToSpawn.Count > 0 ){ // Enquanto tenho inimigos para Spawnar
+    //             Instantiate(enemiesToSpawn[0], spawnLocation.position, Quaternion.identity);
+    //             // Remove o inimigo da lista de Spawn
+    //             enemiesToSpawn.RemoveAt(0);
+    //             spawnTimer = spawnInterval;
+    //         }
+    //         else
+    //         {
+    //             waveTimer = 0;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         spawnTimer -= Time.fixedDeltaTime;
+    //         waveTimer -= Time.fixedDeltaTime;
+    //     }
+    // }
 
     public void GenerateWave()
     {
         waveValue = currWave * 10; // Valor que podemos gastar a depender do layer?
         GenerateEnemies();
-
-        spawnInterval = waveDuration; // enemiesToSpawn.Count; // Tempo fixo entre cada spawn de inimigo.
-        waveTimer = waveDuration; // Duração de uma onda.
+        
+        // spawnInterval = waveDuration; // enemiesToSpawn.Count; // Tempo fixo entre cada spawn de inimigo.
+        // waveTimer = waveDuration; // Duração de uma onda.
     }
 
     public void GenerateEnemies()
