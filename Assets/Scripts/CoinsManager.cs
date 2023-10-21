03f16fe7b0cf71b7746c364062137afa10dd727e
Renato Laffranchi Falcão <66042074+renatex333@users.noreply.github.com>
2023-10-21 19:10:39 -0300

    Adicionando scripts de UI
    + Adicionando scripts para a atualização da interface do usuário, que agora atualiza stats, moedas, xp, nível e nome

---
+++ Assets/Scripts/CoinsManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CoinsManager : MonoBehaviour
+{
+    public Player player;
+    public TextMeshProUGUI coinsCount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // coinsCount.text = player.entity.coins.ToString();
+    }
+}
